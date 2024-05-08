ef1651dddf76be459817dd71f7d7f80cdd028e05
simon.reilich137 <simon.reilich137@gmail.com>
2024-05-08 20:39:04 +0000

    added animation for page transitions

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,9 @@ export default {
   data() {
     return {
       selected: 'dashboard',
-      selectorY: 200
+      selectorY: 188,
+      selectorYcurrent: 188,
+      selectorYstr: '188px'
     }
   },
   methods: {
@@ -11,32 +13,37 @@ export default {
       this.selected = value;
       switch (value) {
         case 'dashboard':
-        selectorY = 200;
+        this.selectorY = 188;
+        break;
 
         case 'courses':
-        selectorY = 300;
+        this.selectorY = 290;
+        break;
 
         case 'messages':
-        selectorY = 400;
+        this.selectorY = 392;
+        break;
 
         case 'calendar':
-        selectorY = 500;
+        this.selectorY = 496;
+        break;
 
         case 'settings':
-        selectorY = 600;
+        this.selectorY = 600;
+        break;
       }
-      animate();
+      this.animate();
     },
 
     animate() {
-      console.log('animate');
-      const selector = this.$refs.selector;
-      while (selector.style.top != selectorY) {
-        if (selector.style.top > selectorY) {
-          selector.style.top -= 1;
-        } else if (selector.style.top < selectorY) {
-          selector.style.top += 1;
-        }
+      if (this.selectorYcurrent > this.selectorY) {
+        this.selectorYcurrent -= 2;
+      } else if (this.selectorYcurrent < this.selectorY) {
+        this.selectorYcurrent += 2;
+      }
+      this.selectorYstr = this.selectorYcurrent.toString() + 'px';
+      if (this.selectorYcurrent != this.selectorY) {
+        setTimeout(this.animate, 5)
       }
     }
   },
@@ -46,11 +53,11 @@ export default {
 <template>
   <div class="desktop">
     <h1 class="logoText poppins-semibold">TimeTable</h1>
-    <a class="poppins-medium" @click="select('dashboard')">Dashboard</a>
-    <a class="poppins-medium" @click="select('courses')">Kurse</a>
-    <a class="poppins-medium" @click="select('messages')">Nachrichten</a>
-    <a class="poppins-medium" @click="select('calendar')">Kalender</a>
-    <a class="poppins-medium" @click="select('settings')">Einstellungen</a>
+    <button class="poppins-medium" @click="select('dashboard')">Dashboard</button>
+    <button class="poppins-medium" @click="select('courses')">Kurse</button>
+    <button class="poppins-medium" @click="select('messages')">Nachrichten</button>
+    <button class="poppins-medium" @click="select('calendar')">Kalender</button>
+    <button class="poppins-medium" @click="select('settings')">Einstellungen</button>
   </div>
   <div class="selector"></div>
 </template>
@@ -63,34 +70,42 @@ export default {
   height: 100%;
   top: 0;
   left: 0;
-  z-index: -100;
 }
 
-.desktop > h1, a {
+.desktop > h1, button {
   margin-block: 4rem;
   margin-inline: 2rem;
   text-align: left;
   display: block;
   font-family: 'Poppins';
   color: #0d0d0d;
+  background-color: transparent;
+  border: none;
+  transition: ease-in-out 0.2s;
 }
 
-.desktop > a {
+.desktop > h1 {
+  font-size: 3rem;
+}
+
+.desktop > button {
   font-size: 1.5rem;
 }
 
-.desktop > h1:hover, a:hover {
+.desktop > h1:hover, button:hover {
   scale: 1.2;
+  translate: 2rem;
 }
 
 .selector {
   background-color: #ff8d4c;
   width: 4rem;
   height: 4rem;
-  z-index: -99;
   position: fixed;
   border-radius: 100%;
   box-shadow: #ff8e4c1a 0px 2px 5px 0px, #ff8d4c17 0px 9px 9px 0px, #ff8d4c0c 0px 20px 12px 0px, #ff8d4c02 0px 36px 14px 0px;
+  top: v-bind(selectorYstr);
+  left: 21rem
 }
 
 @media only screen and (max-width: 1200px) {
@@ -98,7 +113,7 @@ export default {
     width: 10rem;
   }
 
-  .desktop > h1, a {
+  .desktop > h1, button {
     display: none;
   }
 }
