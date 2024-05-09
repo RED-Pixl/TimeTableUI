86fbb8630f297bb3257a97c07f23666616a85cc8
simon.reilich137 <simon.reilich137@gmail.com>
2024-05-09 09:54:05 +0000

    added the icons to the menue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -17,19 +17,19 @@ export default {
         break;
 
         case 'courses':
-        this.selectorY = 290;
+        this.selectorY = 292;
         break;
 
         case 'messages':
-        this.selectorY = 392;
+        this.selectorY = 396;
         break;
 
         case 'calendar':
-        this.selectorY = 496;
+        this.selectorY = 500;
         break;
 
         case 'settings':
-        this.selectorY = 600;
+        this.selectorY = 604;
         break;
       }
       this.animate();
@@ -52,14 +52,29 @@ export default {
 
 <template>
   <div class="desktop">
-    <h1 class="logoText poppins-semibold">TimeTable</h1>
-    <button class="poppins-medium" @click="select('dashboard')">Dashboard</button>
-    <button class="poppins-medium" @click="select('courses')">Kurse</button>
-    <button class="poppins-medium" @click="select('messages')">Nachrichten</button>
-    <button class="poppins-medium" @click="select('calendar')">Kalender</button>
-    <button class="poppins-medium" @click="select('settings')">Einstellungen</button>
-  </div>
   <div class="selector"></div>
+    <h1 class="logoText poppins-semibold">TimeTable</h1>
+    <div class="menue-link">
+    <button class="poppins-medium" @click="select('dashboard')">Dashboard</button>
+    <img src="/home.png" @click="select('dashboard')">
+    </div>
+    <div class="menue-link">
+    <button class="poppins-medium" @click="select('courses')">Kurse</button>
+    <img src="/school.png" @click="select('courses')">
+    </div>
+    <div class="menue-link">
+    <button class="poppins-medium" @click="select('messages')">Nachrichten</button>
+    <img src="/chat.png" @click="select('messages')">
+    </div>
+    <div class="menue-link">
+    <button class="poppins-medium" @click="select('calendar')">Kalender</button>
+    <img src="/calendar_month.png" @click="select('calendar')">
+    </div>
+    <div class="menue-link">
+    <button class="poppins-medium" @click="select('settings')">Einstellungen</button>
+    <img src="/settings.png" @click="select('settings')">
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -72,7 +87,7 @@ export default {
   left: 0;
 }
 
-.desktop > h1, button {
+.desktop > h1 {
   margin-block: 4rem;
   margin-inline: 2rem;
   text-align: left;
@@ -82,14 +97,32 @@ export default {
   background-color: transparent;
   border: none;
   transition: ease-in-out 0.2s;
-}
-
-.desktop > h1 {
   font-size: 3rem;
 }
 
-.desktop > button {
+.menue-link {
+  margin-block: 4rem;
+  margin-inline: 2rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.menue-link > button {
+  text-align: left;
+  font-family: 'Poppins';
+  color: #0d0d0d;
+  background-color: transparent;
+  border: none;
   font-size: 1.5rem;
+  transition: ease-in-out 0.2s;
+}
+
+.menue-link > img {
+  width: 2.5rem;
+  height: 2.5rem;
+  position: relative;
+  z-index: 1;
+  transition: ease-in-out 0.2s;
 }
 
 .desktop > h1:hover, button:hover {
@@ -105,16 +138,28 @@ export default {
   border-radius: 100%;
   box-shadow: #ff8e4c1a 0px 2px 5px 0px, #ff8d4c17 0px 9px 9px 0px, #ff8d4c0c 0px 20px 12px 0px, #ff8d4c02 0px 36px 14px 0px;
   top: v-bind(selectorYstr);
-  left: 21rem
+  left: 21rem;
 }
 
 @media only screen and (max-width: 1200px) {
   .desktop {
-    width: 10rem;
+    width: 8rem;
   }
 
-  .desktop > h1, button {
+  .menue-link > button {
     display: none;
+  }
+
+  .desktop > h1 {
+    color: transparent;
+  }
+
+  .menue-link > img {
+    margin-inline: auto;
+  }
+
+  .selector {
+    left: 5rem;
   }
 }
 </style>
